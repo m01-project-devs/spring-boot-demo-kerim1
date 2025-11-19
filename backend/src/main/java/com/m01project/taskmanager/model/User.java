d6fd8c42cf61c2b6cb80d1eda48e77526b46ce32
Kerimrx <kingcobra0118@.com>
2025-11-19 16:34:20 +0530

    DEV-5 Implemented entity model(homework-7)

---
+++ backend/src/main/java/com/m01project/taskmanager/model/User.java
@@ -0,0 +1,34 @@
+package com.m01project.taskmanager.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+
+}
+
+
