a56bf00ca8f49f27c6914da89263333ed6192c0f
Kerimrx <kingcobra0118@.com>
2025-11-21 22:22:54 +0530

    User entity with the password field added and added User entity class

--- backend/src/main/java/com/m01project/taskmanager/TaskManagerApplication.java
+++ backend/src/main/java/com/m01project/taskmanager/TaskManagerApplication.java
@@ -3,11 +3,3 @@ package com.m01project.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TaskManagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TaskManagerApplication.class, args);
-	}
-
-}
