84ccab87574ffc4d5f1ecb07da6bf06751aebc31
Kerimrx <kingcobra0118@.com>
2025-11-21 22:35:17 +0530

    Created index for email column used bitint

--- backend/src/main/resources/db/migration/V1__create_user_table.sql
+++ backend/src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+                       id BIGSERIAL PRIMARY KEY,
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE INDEX idx_users_email ON users(email);
